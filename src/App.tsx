82f2d836b929eeee672cafd23f9e42e56ec6932a
JazibShakoor <113052211+JazibShakoor@users.noreply.github.com>
2022-10-20 19:03:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, lazy, Fragment } from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy, Fragment, ReactElement } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import UpdateForm from './Pages/Form/UpdateForm';
 import Donut from './Ui/donut';
@@ -9,15 +9,19 @@ const Home = lazy(() => import('./Pages/Home/Home'));
 const Form = lazy(() => import('./Pages/Form/Form'));
 
 const App: React.FC = () => {
+  const RoutesComponents: {path: string, element: ReactElement}[] = [
+    {path: '/', element: <Home />},
+    {path: '/MuneItem', element: <MuneItem />},
+    {path: '/Form', element: <Form />},
+    {path: 'MuneItem/UpdateForm/:id', element: <UpdateForm />}
+  ]
 
   return (
     <Fragment>
       <Suspense fallback={<div><Donut /></div>}>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/MuneItem' element={<MuneItem />}></Route>
-          <Route path='/Form' element={<Form />}></Route>
-          <Route path='MuneItem/UpdateForm/:id' element={<UpdateForm />}></Route>
+          {RoutesComponents.map((props) => 
+          <Route path={props.path} element={props.element}></Route>)}
         </Routes>
       </Suspense>
     </Fragment>
